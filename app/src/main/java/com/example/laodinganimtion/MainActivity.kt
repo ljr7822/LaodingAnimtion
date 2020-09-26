720c0af47726e8fa8342c2f09895cba30382c7a3
iwenLjr <1816807688@qq.com>
2020-09-26 11:49:49 +0800

    动画绘制完毕，下一步绘制加载成功与否动画

--- app/src/main/java/com/example/laodinganimtion/MainActivity.kt
+++ app/src/main/java/com/example/laodinganimtion/MainActivity.kt
@@ -1,12 +1,37 @@
 package com.example.laodinganimtion
 
+import android.animation.ValueAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mProgress.result = ProgressView.SUCCESS
+        // 2. 设置回调函数（谁实现给谁）
+        mProgress.callBack = {
+            // 做下载任务
+            downloadData()
+        }
+    }
+
+    // 自定义下载
+    private fun downloadData(){
+        ValueAnimator.ofFloat(0f,1.0f).apply {
+            duration = 2000
+            addUpdateListener {
+                (it.animatedValue as Float).also {value ->
+                    mProgress.progress = value
+                    if (value == 1.0f){
+                        mProgress.result = ProgressView.SUCCESS
+                    }
+                }
+
+            }
+            start()
+        }
     }
 }
--- app/src/main/java/com/example/laodinganimtion/ProgressView.kt
+++ app/src/main/java/com/example/laodinganimtion/ProgressView.kt
@@ -6,18 +6,28 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
-import android.graphics.RectF
 import android.util.AttributeSet
+import android.util.Log
 import android.view.MotionEvent
 import android.view.View
+import kotlin.math.log
 
 /**
- *
+ * 长方形变圆角矩形，再回缩
  * @author iwen大大怪
  * Create to 2020/9/26 8:53
  *
  */
-class ProgressView: View {
+class ProgressView : View {
+    /**
+     * 高阶函数：
+     */
+    // 1. 定义函数类型:
+    // 参数：就是要返回的数据类型
+    // 返回值：是否需要对方给我返回结果
+    var callBack:(() -> Unit)? = null
+    // 2. 设置回调函数（谁实现给谁）
+    // 3. 调用
 
     //定义变成半圆的动画因子
     private var cornerRadius = 0f
@@ -25,6 +35,25 @@ class ProgressView: View {
     //定义中间靠拢的动画因子
     private var tranx = 0f
 
+    //定义进度的变化因子 0-1.0
+    var progress = 0f
+        set(value) {
+            field = value
+            //刷新
+            invalidate()
+
+            if (progress == 1.0f){
+                startFinishAnim()
+            }
+        }
+    // 保存下载结果
+    var result = UNKNOW
+    // 伴生对象
+    companion object {
+        val SUCCESS = 0
+        val FAILUEF = 1
+        val UNKNOW = 2
+    }
     constructor(context: Context?) : super(context)
     constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
 
@@ -34,21 +63,36 @@ class ProgressView: View {
         style = Paint.Style.FILL
     }
 
-    override fun onDraw(canvas: Canvas?) {
-        canvas?.drawRoundRect(
-            tranx,
-            0f,
-            width-tranx,
-            height.toFloat(),
-            cornerRadius,
-            cornerRadius,
-            mRoundRectPaint
-        )
+    // 顶部矩形画笔
+    private val mRoundRectPaint1 = Paint().apply {
+        color = Color.BLUE
+        style = Paint.Style.FILL
     }
 
-    private fun startFinishAnim(){
+    override fun onDraw(canvas: Canvas?) {
+        // 绘制背景--原始进度
+        if (progress < 1.0f){
+            canvas?.drawRoundRect(tranx, 0f, width.toFloat(), height.toFloat(), 0f, 0f, mRoundRectPaint)
+        }
+        // 绘制前景--下载进度
+        canvas?.drawRoundRect(tranx,0f,progress*width-tranx,height.toFloat(), cornerRadius, cornerRadius, mRoundRectPaint1)
+
+        // 判断是否要结果
+        if (tranx == (width-height)/2f){
+            if (result == SUCCESS || result == UNKNOW){
+                // 下载成功
+
+            }else{
+                // 下载失败
+
+            }
+        }
+    }
+
+    // 开始动画
+    private fun startFinishAnim() {
         // 变成圆角矩形
-        val toCircleAnim = ValueAnimator.ofFloat(0f, height/2f).apply {
+        val toCircleAnim = ValueAnimator.ofFloat(0f, height / 2f).apply {
             duration = 1000
             addUpdateListener {
                 cornerRadius = it.animatedValue as Float
@@ -56,7 +100,7 @@ class ProgressView: View {
             }
         }
         // 中间靠拢的动画
-        val moveToCenterAnim = ValueAnimator.ofFloat(0f, (width-height)/2f).apply {
+        val moveToCenterAnim = ValueAnimator.ofFloat(0f, (width - height) / 2f).apply {
             duration = 2000
             addUpdateListener {
                 tranx = it.animatedValue as Float
@@ -72,8 +116,11 @@ class ProgressView: View {
 
     // 点击视图
     override fun onTouchEvent(event: MotionEvent?): Boolean {
-        if(event?.action == MotionEvent.ACTION_DOWN){
-            startFinishAnim()
+        if (event?.action == MotionEvent.ACTION_DOWN) {
+            // 3. 调用
+            callBack?.let {
+                it()
+            }
         }
         return true
     }
